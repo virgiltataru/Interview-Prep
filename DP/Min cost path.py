1ddc91041ffedd5149c12ab2baec35b21f5906b3
Virgil <virgiltataru97@gmail.com>
2018-10-10 14:04:48 +0800

    Coin change

---
+++ DP/Min cost path.py
@@ -0,0 +1,9 @@
+def minCost(cost, m, n):
+    if (n < 0 or m < 0):
+        return sys.maxsize
+    elif (m == 0 and n == 0):
+        return cost[m][n]
+    else:
+        return cost[m][n] + min( minCost(cost, m-1, n-1),
+                                minCost(cost, m-1, n),
+                                minCost(cost, m, n-1) )
