4b78a6f333c46aa1300974f1c0402d39e316849f
Virgil <virgiltataru97@gmail.com>
2018-10-12 15:01:04 +0800

    BT

---
+++ Binary Trees/LCA.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+    """
+    :type root: TreeNode
+    :type p: TreeNode
+    :type q: TreeNode
+    :rtype: TreeNode
+    """
+
+        if not root or root in [p,q]:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+        else:
+            return left or right
