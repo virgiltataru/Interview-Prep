19acdb6a0c92f8a93fd77e68aca3707ce6a0a265
Virgil <virgiltataru97@gmail.com>
2018-10-14 20:25:54 +0800

    bst

---
+++ Binary Trees/Is balanced.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        def dfs(p):
+            if not p:
+                return 0
+
+            left = dfs(p.left)
+            right = dfs(p.right)
+            if left == -1 or right == -1:
+                return -1
+            if abs(left - right) > 1:
+                return -1
+            return 1 + max(left, right)
+        if dfs(root) == -1:
+            return False
+        return True
