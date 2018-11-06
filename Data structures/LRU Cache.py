7dae5100809da8b26b45d3a18aa0770f8e265504
Virgil <virgiltataru97@gmail.com>
2018-11-06 13:49:55 +0800

    ref

--- Data structures/LRU Cache.py
+++ Data structures/LRU Cache.py
@@ -1,21 +1,21 @@
 class LRUCache:
-def __init__(self, capacity):
-    self.deque = collections.deque([])
-    self.dic = {}
-    self.capacity = capacity
+    def __init__(self, capacity):
+        self.deque = collections.deque([])
+        self.dic = {}
+        self.capacity = capacity
 
-def get(self, key):
-    if key not in self.dic:
-        return -1
-    self.deque.remove(key)
-    self.deque.append(key)
-    return self.dic[key]
-
-def set(self, key, value):
-    if key in self.dic:
+    def get(self, key):
+        if key not in self.dic:
+            return -1
         self.deque.remove(key)
-    elif len(self.dic) == self.capacity:
-        v = self.deque.popleft()  # remove the Least Recently Used element
-        self.dic.pop(v)
-    self.deque.append(key)
-    self.dic[key] = value
+        self.deque.append(key)
+        return self.dic[key]
+
+    def set(self, key, value):
+        if key in self.dic:
+            self.deque.remove(key)
+        elif len(self.dic) == self.capacity:
+            v = self.deque.popleft()  # remove the Least Recently Used element
+            self.dic.pop(v)
+        self.deque.append(key)
+        self.dic[key] = value
